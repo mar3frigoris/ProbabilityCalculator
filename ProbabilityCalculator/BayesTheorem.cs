44976f13434b7eb2e4ba97c950a3e41f00e86231
nika <nvjagrdsohgjb@gmail.com>
2022-09-01 22:00:20 +0300

    Initial Commit

---
+++ ProbabilityCalculator/BayesTheorem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace ProbabilityCalculator;
+
+public static class BayesTheorem
+{
+    // probabilityOfH - probability of HYPOTHESIS.
+    // P(H)
+    // Prior probability the hyphothesis is true.
+    // How likely you thought you were to have the desiase prior to the result.
+
+    // probabilityOfEGivenH - probability of EVENT given HYPOTHESIS
+    // P(E|H)
+    // Probability of the event given the hyphothesis is true.
+    // The probability you were tested posetive if you had the desease.
+    public static decimal Probability(decimal probabilityOfH, decimal probabilityOfEGivenH)
+    {
+        // P(E|H) * P(H)
+        // How likely the hyphothesis(H) is true given the event(E).
+        // How likely you have the desease.
+        var probabilityOfHAndE = probabilityOfH * probabilityOfEGivenH;
+
+        // P(-H)
+        var probabilityOfNotH = 1 - probabilityOfH;
+        //  P(E|-H)
+        var probabilityOfEGivenNotH = 1 - probabilityOfEGivenH;
+
+        // P(E) = P(H) * P(E|H) + P(-H) * P(E|-H)
+        // Total probability of the event occurring.
+        // Probability of testing posetive.
+        var probabilityOfE = (probabilityOfH * probabilityOfEGivenH) + (probabilityOfNotH * probabilityOfEGivenNotH);
+
+        // The result.
+        var probabilityOfHGivenE = probabilityOfHAndE / probabilityOfE;
+        return probabilityOfHGivenE;
+    }
+
+    public static decimal ProbabilityInPercents(decimal probabilityOfH, decimal probabilityOfEGivenH)
+    {
+        return Probability(probabilityOfH, probabilityOfEGivenH) * 100;
+    }
+}
---
+++ ProbabilityTests/BayesTheoremTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ProbabilityCalculator;
+
+namespace ProbabilityTests;
+
+public class BayesTheoremTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void RareDeseaseTestedPosetive()
+    {
+        // Hyphothesis: The desiase affects 0.1% of the population.
+        // 0.1% - 1 of 1000 = 0.001
+        var probabilityOfHyphothesis = 0.001m;
+        // Event: You have tested posetive.
+        // The accuracy of the test is 99%.
+        var probabilityOfEventGivenHyphothesis = 0.99m;
+
+        var probability = BayesTheorem.ProbabilityInPercents(probabilityOfHyphothesis, probabilityOfEventGivenHyphothesis);
+        var roundedProbability = Math.Round(probability);
+        Assert.AreEqual(9, roundedProbability);
+    }
+
+    [Test]
+    public void RareDeseaseTestedPosetiveTwice()
+    {
+        // Hyphothesis: The desiase affects 0.1% of the population.
+        // 0.1% - 1 of 1000 = 0.001
+        var probabilityOfHyphothesis = 0.001m;
+        // Event: You have tested posetive.
+        // The accuracy of the test is 99%.
+        var probabilityOfEventGivenHyphothesis = 0.99m;
+        // Counting probability after you were tested posetive once.
+        var probabilityAfterFirstTest = BayesTheorem.ProbabilityInPercents(probabilityOfHyphothesis, probabilityOfEventGivenHyphothesis);
+        // Probability after you were tested posetive twice.
+        var probabilityAfterSecondTest = BayesTheorem.ProbabilityInPercents(probabilityAfterFirstTest / 100, probabilityOfEventGivenHyphothesis);
+
+        var roundedProbability = Math.Round(probabilityAfterSecondTest);
+        Assert.AreEqual(91, roundedProbability);
+    }
+}
+
